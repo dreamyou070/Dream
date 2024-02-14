1c8c05e957d90d26a7ef0e21b3f94b3ea2e99815
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 19:39:35 +0900

    chagne git ignore

--- train_DRAEM.py
+++ train_DRAEM.py
@@ -19,41 +19,45 @@ def weights_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
+
 def train_on_device(obj_names, args):
 
+    print(f' step 1. check result and logging path')
     if not os.path.exists(args.checkpoint_path):
         os.makedirs(args.checkpoint_path)
-
     if not os.path.exists(args.log_path):
         os.makedirs(args.log_path)
 
+    print(f' step 2. which to train')
     for obj_name in obj_names:
-        run_name = 'DRAEM_test_'+str(args.lr)+'_'+str(args.epochs)+'_bs'+str(args.bs)+"_"+obj_name+'_'
 
+        run_name = 'DRAEM_test_'+str(args.lr)+'_'+str(args.epochs)+'_bs'+str(args.bs)+"_"+obj_name+'_'
         visualizer = TensorboardVisualizer(log_dir=os.path.join(args.log_path, run_name+"/"))
 
+        print(f' (2.1) make model')
         model = ReconstructiveSubNetwork(in_channels=3, out_channels=3)
         model.cuda()
         model.apply(weights_init)
 
-        model_seg = DiscriminativeSubNetwork(in_channels=6, out_channels=2)
+        print(f' (2.2) segmentation model')
+        model_seg = DiscriminativeSubNetwork(in_channels=6,ut_channels=2)
         model_seg.cuda()
         model_seg.apply(weights_init)
 
-        optimizer = torch.optim.Adam([
-                                      {"params": model.parameters(), "lr": args.lr},
+        print(f' (2.3) optimizer')
+        optimizer = torch.optim.Adam([{"params": model.parameters(), "lr": args.lr},
                                       {"params": model_seg.parameters(), "lr": args.lr}])
 
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer,[args.epochs*0.8,args.epochs*0.9],gamma=0.2, last_epoch=-1)
-
         loss_l2 = torch.nn.modules.loss.MSELoss()
         loss_ssim = SSIM()
         loss_focal = FocalLoss()
 
-        dataset = MVTecDRAEMTrainDataset(args.data_path + obj_name + "/train/good/", args.anomaly_source_path, resize_shape=[256, 256])
-
-        dataloader = DataLoader(dataset, batch_size=args.bs,
-                                shuffle=True, num_workers=16)
+        print(f' (2.4) dataset')
+        dataset = MVTecDRAEMTrainDataset(args.data_path + obj_name + "/train/good/",
+                                         args.anomaly_source_path,
+                                         resize_shape=[256, 256])
+        dataloader = DataLoader(dataset, batch_size=args.bs,shuffle=True, num_workers=16)
 
         n_iter = 0
         for epoch in range(args.epochs):
@@ -69,10 +73,16 @@ def train_on_device(obj_names, args):
                 out_mask = model_seg(joined_in)
                 out_mask_sm = torch.softmax(out_mask, dim=1)
 
-                l2_loss = loss_l2(gray_rec,gray_batch)
-                ssim_loss = loss_ssim(gray_rec, gray_batch)
 
-                segment_loss = loss_focal(out_mask_sm, anomaly_mask)
+                l2_loss = loss_l2(gray_rec,
+                                  gray_batch)
+
+                ssim_loss = loss_ssim(gray_rec,
+                                      gray_batch)
+
+
+                segment_loss = loss_focal(out_mask_sm,
+                                          anomaly_mask)
                 loss = l2_loss + ssim_loss + segment_loss
 
                 optimizer.zero_grad()
@@ -115,43 +125,14 @@ if __name__=="__main__":
     parser.add_argument('--log_path', action='store', type=str, required=True)
     parser.add_argument('--visualize', action='store_true')
     args = parser.parse_args()
-    obj_batch = [['capsule'],
-                 ['bottle'],
-                 ['carpet'],
-                 ['leather'],
-                 ['pill'],
-                 ['transistor'],
-                 ['tile'],
-                 ['cable'],
-                 ['zipper'],
-                 ['toothbrush'],
-                 ['metal_nut'],
-                 ['hazelnut'],
-                 ['screw'],
-                 ['grid'],
-                 ['wood']]
-
+    obj_batch = [['capsule'],['bottle'],['carpet'],['leather'],['pill'], ['transistor'],['tile'],['cable'],['zipper'],
+                 ['toothbrush'],['metal_nut'],['hazelnut'],['screw'],['grid'],['wood']]
     if int(args.obj_id) == -1:
-        obj_list = ['capsule',
-                     'bottle',
-                     'carpet',
-                     'leather',
-                     'pill',
-                     'transistor',
-                     'tile',
-                     'cable',
-                     'zipper',
-                     'toothbrush',
-                     'metal_nut',
-                     'hazelnut',
-                     'screw',
-                     'grid',
-                     'wood'
-                     ]
+        obj_list = ['capsule','bottle','carpet','leather','pill','transistor','tile',
+                     'cable','zipper','toothbrush','metal_nut','hazelnut','screw','grid','wood']
         picked_classes = obj_list
     else:
         picked_classes = obj_batch[int(args.obj_id)]
-
     with torch.cuda.device(args.gpu_id):
         train_on_device(picked_classes, args)
 
