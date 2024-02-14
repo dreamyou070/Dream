4fb59c5587247ecf7714b3ebf224cdd7f4b3e806
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 19:41:07 +0900

    Update train_DREAM.sh

--- scripts/train_DREAM.sh
+++ scripts/train_DREAM.sh
@@ -1,10 +1,10 @@
 python ../train_DREAM.py \
  --gpu_id 0 \
  --obj_id -1 \
- --lr 0.0001
- --bs 8
- --epochs 700
- --data_path ../../../MyData/anomaly_detection/MVTec
- --anomaly_source_path ./datasets/dtd/images/
+ --lr 0.0001 \
+ --bs 8 \
+ --epochs 700 \
+ --data_path ../../../MyData/anomaly_detection/MVTec \
+ --anomaly_source_path ./datasets/dtd/images/ \
  --checkpoint_path ./result/ \
  --log_path ./logs/
