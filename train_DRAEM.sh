572f450de7cce4170c719386d27f44e894130373
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 15:21:36 +0900

    Update train_DRAEM.sh

--- train_DRAEM.sh
+++ train_DRAEM.sh
@@ -4,7 +4,7 @@ python train_DRAEM.py \
  --lr 0.0001 \
  --bs 8 \
  --epochs 700 \
- --data_path ../../../MyData/anomaly_detection/MVTec \
+ --data_path ../../MyData/anomaly_detection/MVTec \
  --anomaly_source_path /home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/anomal_source \
  --checkpoint_path ./result/ \
  --log_path ./logs/
